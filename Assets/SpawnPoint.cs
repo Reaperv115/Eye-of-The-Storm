80e5833320ea18d3a55019acf34c51d54a7da2c8
Reaper <rjs5785@hotmail.com>
2020-08-31 10:17:59 -0400

    enemies spawn at random spawn points around the platform.

---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] spawnpoints;
+    [SerializeField]
+    GameObject enemy;
+
+    int pointtospawnAt;
+    float spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointtospawnAt = Random.Range(0, spawnpoints.Length);
+        Instantiate(enemy, spawnpoints[pointtospawnAt].position, transform.rotation);
+        spawnTimer = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0.0f)
+        {
+            pointtospawnAt = Random.Range(0, spawnpoints.Length);
+            Instantiate(enemy, spawnpoints[pointtospawnAt].transform.position, transform.rotation);
+            spawnTimer = 2.0f;
+        }
+    }
+}
