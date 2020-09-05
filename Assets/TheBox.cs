3376585c1c7834a5a972fa1ba100ce7c148bb3bc
Reaper <rjs5785@hotmail.com>
2020-09-05 10:55:06 -0400

    everything seems to be working properly again

---
+++ Assets/TheBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBox : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] weapons;
+    [SerializeField]
+    Transform startingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == "Player")
+        {
+            int randomweaponIndex = Random.Range(0, weapons.Length);
+            Instantiate(weapons[randomweaponIndex], startingPosition.position, startingPosition.rotation);
+        }
+    }
+}
